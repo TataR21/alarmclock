2eb34159411e45d448e87e5de5d80868a060548f
TataR21 <m.tatarski96@gmail.com>
2018-06-09 18:57:06 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace budzik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trybCichyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(117, 12);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(250, 49);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Location = new System.Drawing.Point(12, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(463, 290);
+            this.panel1.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(117, 67);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Dodaj alarm";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "ghjgbvgnc";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Alarm";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClosed += new System.EventHandler(this.notifyIcon1_BalloonTipClosed);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zakończToolStripMenuItem,
+            this.trybCichyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(129, 48);
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            this.zakończToolStripMenuItem.Click += new System.EventHandler(this.zakończToolStripMenuItem_Click);
+            // 
+            // trybCichyToolStripMenuItem
+            // 
+            this.trybCichyToolStripMenuItem.Name = "trybCichyToolStripMenuItem";
+            this.trybCichyToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.trybCichyToolStripMenuItem.Text = "Tryb cichy";
+            this.trybCichyToolStripMenuItem.Click += new System.EventHandler(this.trybCichyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 418);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Alarm";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem trybCichyToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace budzik
+{
+    public partial class Form1 : Form
+    {
+        int leftbox = 210;
+        int left = 3;
+        int leftbox2 = 230;
+        int lefttext = 280;
+        Point point;
+        bool muzyka = true;
+
+        List<DateTimePicker> datetimepicker = new List<DateTimePicker>();
+        List<CheckBox> checkbox = new List<CheckBox>();
+        List<DateTime> datetime = new List<DateTime>();
+        List<Panel> panel = new List<Panel>();
+        List<CheckBox> checkbox2 = new List<CheckBox>();
+        List<TextBox> textbox = new List<TextBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            wczytajczas();
+
+            timer3.Interval = 200;
+            timer3.Start();
+            timer3.Tick += new EventHandler(timer_Tick3);
+
+            this.FormClosing += new FormClosingEventHandler(Form1_Closing);
+
+            this.notifyIcon1.DoubleClick += new EventHandler(notifyclick);
+
+            timer1.Interval = 1000;
+            timer1.Start();
+            timer1.Tick += new EventHandler(zegar);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dodajalarm();
+        }
+
+        private void zakończToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            zapiszczas();
+            Application.Exit();
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.ApplicationExitCall)
+            {
+                notifyIcon1.Visible = true;
+                this.Hide();
+                e.Cancel = true;
+            }
+        }
+
+        void timer_Tick3(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkbox.Count; i++)
+            {
+                if (datetimepicker[i].Value <= DateTime.Now && checkbox[i].Checked == true && datetimepicker[i].Value.AddSeconds(5) >= DateTime.Now)
+                {
+                    if (String.IsNullOrEmpty(textbox[i].Text))
+                    {
+                        notifyIcon1.BalloonTipText = "Alarm ";
+                    }
+                    else
+                    {
+                        notifyIcon1.BalloonTipText = textbox[i].Text;
+                    }
+                    notifyIcon1.ShowBalloonTip(0);
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                    player.SoundLocation = @"../../save/2.wav";
+                    player.Load();
+                    if (muzyka == true)
+                    {
+                        player.Play();
+                    }
+                    timer3.Stop();
+                    notifyIcon1.BalloonTipClicked += new EventHandler(powiadomienie);
+
+                    checkbox[i].Checked = false;
+                }
+
+                if (checkbox2[i].Checked == true)
+                {
+                    for (int x = i; x < datetimepicker.Count; x++)
+                    {
+                        if (x + 1 < datetimepicker.Count)
+                        {
+                            datetimepicker[x].Value = datetimepicker[x + 1].Value;
+                            textbox[x].Text = textbox[x + 1].Text;
+                        }
+                    }
+
+                    checkbox2[i].Checked = false;
+                    datetimepicker.RemoveAt(datetimepicker.Count - 1);
+
+                    checkbox.RemoveAt(checkbox.Count - 1);
+                    textbox.RemoveAt(textbox.Count - 1);
+
+                    checkbox2.RemoveAt(checkbox2.Count - 1);
+                    panel[panel.Count - 1].Controls.Clear();
+                    panel.RemoveAt(panel.Count - 1);
+
+                    panel1.Controls.RemoveAt(panel.Count);
+                }
+            }
+        }
+
+        private void powiadomienie(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = @"../../save/2.wav";
+            player.Stop();
+            timer3.Start();
+        }
+
+        private void notifyclick(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        void zapiszczas()
+        {
+            string[] temp;
+            string[] tekst;
+            temp = new string[datetimepicker.Count];
+            tekst = new string[textbox.Count];
+            for (int i = 0; i < datetimepicker.Count; i++)
+            {
+                temp[i] = datetimepicker[i].ToString();
+                temp[i] = temp[i].Substring(44);
+                tekst[i] = textbox[i].Text;
+            }
+            string path = System.IO.Directory.GetCurrentDirectory();
+            System.IO.File.WriteAllLines(@"../../save/save.txt", temp);
+            System.IO.File.WriteAllLines(@"../../save/text.txt", tekst);
+        }
+
+        void wczytajczas()
+        {
+            DateTime odczyt;
+            string[] temp = System.IO.File.ReadAllLines(@"../../save/save.txt");
+            string[] tekst = System.IO.File.ReadAllLines(@"../../save/text.txt");
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                dodajalarm();
+                odczyt = DateTime.Parse(temp[i]);
+                datetimepicker[i].Value = odczyt;
+                textbox[i].Text = tekst[i];
+            }
+        }
+
+        void dodajalarm()
+        {
+            if (panel.Count != 0)
+            {
+                point = panel[panel.Count - 1].Location;
+                point.Y += 40;
+            }
+            else
+            {
+                point.X = 0;
+                point.Y = 0;
+            }
+
+            //panel2
+            Panel newpanel = new Panel();
+            newpanel.Left = 0;
+            newpanel.Width = 450;
+            newpanel.Height = 32;
+            panel.Add(newpanel);
+            newpanel.Location = point;
+            panel1.Controls.Add(newpanel);
+
+            //datetimepicker
+            DateTimePicker newdatetimepicker = new DateTimePicker();
+            newdatetimepicker.Left = left;
+            newdatetimepicker.Top = 0;
+            datetimepicker.Add(newdatetimepicker);
+            newpanel.Controls.Add(newdatetimepicker);
+            newdatetimepicker.Format = DateTimePickerFormat.Custom;
+            newdatetimepicker.CustomFormat = "MM/dd/yyyy HH:mm:ss";
+
+            //checkbox
+            CheckBox newcheckbox = new CheckBox();
+            newcheckbox.Left = leftbox;
+            newcheckbox.Top = 5;
+            checkbox.Add(newcheckbox);
+            newpanel.Controls.Add(newcheckbox);
+            newcheckbox.Text = null;
+            newcheckbox.AutoSize = true;
+
+            //checkbox usuwanie
+            CheckBox newcheckbox2 = new CheckBox();
+            newcheckbox2.Left = leftbox2;
+            newcheckbox2.Top = 0;
+            checkbox2.Add(newcheckbox2);
+            newpanel.Controls.Add(newcheckbox2);
+            newcheckbox2.Text = "X";
+            newcheckbox2.AutoSize = true;
+            newcheckbox2.Appearance = Appearance.Button;
+
+            //textbox
+            TextBox newtextbox = new TextBox();
+            newtextbox.Left = lefttext;
+            newtextbox.Top = 0;
+            newtextbox.Width = 100;
+            textbox.Add(newtextbox);
+            newpanel.Controls.Add(newtextbox);
+            newtextbox.Text = "Alarm";
+        }
+
+        void zegar(object sender, EventArgs e)
+        {
+            dateTimePicker2.Value = DateTime.Now;
+        }
+
+        private void trybCichyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (muzyka == true)
+            {
+                muzyka = false;
+                contextMenuStrip1.Items[1].Text = "Dzwięk";
+            }
+            else
+            {
+                muzyka = true;
+                contextMenuStrip1.Items[1].Text = "Tryb cichy";
+            }
+        }
+
+        private void notifyIcon1_BalloonTipClosed(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = @"../../save/2.wav";
+            player.Stop();
+            timer3.Start();
+        }
+    }
+}
